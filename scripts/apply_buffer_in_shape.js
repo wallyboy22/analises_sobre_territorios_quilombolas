5fd924bf09c67de7ef71d51edcf856a068db484f
Wallace Vieira da Silva <49287183+wallyboy22@users.noreply.github.com>
2024-12-11 22:53:32 -0300

    Update apply_buffer_in_shape.js

--- scripts/apply_buffer_in_shape.js
+++ scripts/apply_buffer_in_shape.js
@@ -1,5 +1,5 @@
 // shape
-var quilombolas = ee.FeatureCollection("projects/ee-babecsilva-consultorias/assets/incra_areas_de_quilombolas")//.limit(1);
+var quilombolas = ee.FeatureCollection("projects/ee-babecsilva-consultorias/assets/ISA/incra_areas_de_quilombolas")//.limit(1);
 
 print('quilombolas',quilombolas);
 Map.addLayer(quilombolas,{color:'red'},'quilombolas');
@@ -24,7 +24,7 @@ Map.addLayer(quilombolas_buffer,{color:'green'},'quilombolas_buffer');
 print('quilombolas_somente_buffer',quilombolas_somente_buffer);
 Map.addLayer(quilombolas_somente_buffer,{color:'blue'},'quilombolas_somente_buffer');
 
-var assetId = 'projects/ee-babecsilva-consultorias/assets/';
+var assetId = 'projects/ee-babecsilva-consultorias/assets/ISA/';
 
 Export.table.toAsset({
   collection:quilombolas_buffer,
@@ -55,7 +55,7 @@ Export.table.toDrive({
   collection:quilombolas_somente_buffer,
   description:'incra_areas_de_quilombolas_somente_buffer_10km',
   folder:'analises_sobre_territorios_quilombolas', 
-  fileNamePrefix:'incra_areas_de_quilombolas_buffer_10km',
+  fileNamePrefix:'incra_areas_de_quilombolas_somente_buffer_10km',
   fileFormat:'shp',
 });
 
@@ -63,6 +63,6 @@ Export.table.toDrive({
   collection:quilombolas_somente_buffer,
   description:'csv_incra_areas_de_quilombolas_somente_buffer_10km',
   folder:'analises_sobre_territorios_quilombolas', 
-  fileNamePrefix:'csv_incra_areas_de_quilombolas_buffer_10km',
+  fileNamePrefix:'csv_incra_areas_de_quilombolas_somente_buffer_10km',
   fileFormat:'csv',
 });
